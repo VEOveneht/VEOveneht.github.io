a1498a38602b6bb14485fb8b582a5ac2fea73fc1
VEOveneht <lisanshidqi0@gmail.com>
2024-04-02 19:45:32 +0700

    sidebar but still on going..

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,23 @@
+const nav = document.querySelector("nav");
+const sidebar = document.querySelector(".sidebar");
+const showbtn = document.querySelector(".showbtn");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+
 window.addEventListener("scroll", function() {
-    const nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function showBar() {
+    sidebar.style.display = "flex";
+    sidebar.style.width = "100%";
+    main.style.display = "none";
+    footer.style.display = "none";
+}
+
+function closeBar() {
+    sidebar.style.width = "0px";
+    main.style.filter = "blur()";
+    main.style.display = "block";
+    footer.style.display = "block";
+}
