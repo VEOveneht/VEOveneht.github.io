a2ac43840d78ad791dea39cc2def4b677eb7a52e
VEOveneht <lisanshidqi0@gmail.com>
2024-04-13 02:26:39 +0700

    2024/04/13 2:26 AM

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,53 @@
-const nav = document.querySelector("nav");
+const home = document.getElementById("home");
+const contact = document.getElementById("contact");
+const skills = document.getElementById("skills");
+const cv = document.getElementById("cv");
+const log = console.log;
 
-window.addEventListener("scroll", function () {
-	nav.classList.toggle("sticky", window.scrollY > 0);
+home.addEventListener("mouseenter", () => {
+    contact.style.color = "gray";
+    skills.style.color = "gray";
+    cv.style.color = "gray";
+});
+
+home.addEventListener("mouseleave", () => {
+    contact.style.color = "#ffae17";
+    skills.style.color = "#ffae17";
+    cv.style.color = "#ffae17";
+});
+
+contact.addEventListener("mouseenter", () => {
+    home.style.color = "gray";
+    skills.style.color = "gray";
+    cv.style.color = "gray";
+});
+
+contact.addEventListener("mouseleave", () => {
+    home.style.color = "#ffae17";
+    skills.style.color = "#ffae17";
+    cv.style.color = "#ffae17";
+});
+
+skills.addEventListener("mouseenter", () => {
+    home.style.color = "gray";
+    contact.style.color = "gray";
+    cv.style.color = "gray";
+});
+
+skills.addEventListener("mouseleave", () => {
+    home.style.color = "#ffae17";
+    contact.style.color = "#ffae17";
+    cv.style.color = "#ffae17";
+});
+
+cv.addEventListener("mouseenter", () => {
+    home.style.color = "gray";
+    contact.style.color = "gray";
+    skills.style.color = "gray";
+});
+
+cv.addEventListener("mouseleave", () => {
+    home.style.color = "#ffae17";
+    contact.style.color = "#ffae17";
+    skills.style.color = "#ffae17";
 });
