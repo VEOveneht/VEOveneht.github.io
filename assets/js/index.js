e8db6857e246d3a880406021a1994343fbceec09
VEOveneht <lisanshidqi0@gmail.com>
2024-04-13 15:41:12 +0700

    Nav Bar Lagi... Design Ulang TEROOOOSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,66 @@
-const home = document.getElementById("home");
-const contact = document.getElementById("contact");
-const skills = document.getElementById("skills");
-const cv = document.getElementById("cv");
+const main = document.querySelector("main");
+const home = document.querySelector(".home");
+const contact = document.querySelector(".contact");
+const skills = document.querySelector(".skills");
+const cv = document.querySelector(".cv");
+const sidebar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
 const log = console.log;
 
 home.addEventListener("mouseenter", () => {
-    contact.style.color = "gray";
-    skills.style.color = "gray";
-    cv.style.color = "gray";
+	contact.style.color = "gray";
+	skills.style.color = "gray";
+	cv.style.color = "gray";
 });
 
 home.addEventListener("mouseleave", () => {
-    contact.style.color = "#ffae17";
-    skills.style.color = "#ffae17";
-    cv.style.color = "#ffae17";
+	contact.style.color = "#ffae17";
+	skills.style.color = "#ffae17";
+	cv.style.color = "#ffae17";
 });
 
 contact.addEventListener("mouseenter", () => {
-    home.style.color = "gray";
-    skills.style.color = "gray";
-    cv.style.color = "gray";
+	home.style.color = "gray";
+	skills.style.color = "gray";
+	cv.style.color = "gray";
 });
 
 contact.addEventListener("mouseleave", () => {
-    home.style.color = "#ffae17";
-    skills.style.color = "#ffae17";
-    cv.style.color = "#ffae17";
+	home.style.color = "#ffae17";
+	skills.style.color = "#ffae17";
+	cv.style.color = "#ffae17";
 });
 
 skills.addEventListener("mouseenter", () => {
-    home.style.color = "gray";
-    contact.style.color = "gray";
-    cv.style.color = "gray";
+	home.style.color = "gray";
+	contact.style.color = "gray";
+	cv.style.color = "gray";
 });
 
 skills.addEventListener("mouseleave", () => {
-    home.style.color = "#ffae17";
-    contact.style.color = "#ffae17";
-    cv.style.color = "#ffae17";
+	home.style.color = "#ffae17";
+	contact.style.color = "#ffae17";
+	cv.style.color = "#ffae17";
 });
 
 cv.addEventListener("mouseenter", () => {
-    home.style.color = "gray";
-    contact.style.color = "gray";
-    skills.style.color = "gray";
+	home.style.color = "gray";
+	contact.style.color = "gray";
+	skills.style.color = "gray";
 });
 
 cv.addEventListener("mouseleave", () => {
-    home.style.color = "#ffae17";
-    contact.style.color = "#ffae17";
-    skills.style.color = "#ffae17";
+	home.style.color = "#ffae17";
+	contact.style.color = "#ffae17";
+	skills.style.color = "#ffae17";
 });
+
+function showbar() {
+	sidebar.style.display = "flex";
+	sidebar.style.width = "100%";
+}
+
+function hidebar() {
+	sidebar.style.width = "0%";
+}
