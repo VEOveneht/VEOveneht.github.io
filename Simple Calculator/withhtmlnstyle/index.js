bb28a91d0fab4c2fdbbc2dd0c802ec622e023088
VEOveneht <lisanshidqi0@gmail.com>
2024-05-04 22:13:25 +0700

    Update Fake Project

--- Simple Calculator/withhtmlnstyle/index.js
+++ Simple Calculator/withhtmlnstyle/index.js
@@ -10,27 +10,43 @@ function calculateor() {
     switch (options) {
         case "add":
             total = parseFloat(input1.value) + parseFloat(input2.value);
-            output.innerHTML = total;
-            console.log(total);
+
+            if (isNaN(total)) {
+                output.innerHTML = "Where's the Number?";
+            } else {
+                output.innerHTML = total;
+            }
             break;
         case "minus":
             total = parseFloat(input1.value) - parseFloat(input2.value);
-            output.innerHTML = total;
-            console.log(total);
+            if (isNaN(total)) {
+                output.innerHTML = "Where's the Number?";
+            } else {
+                output.innerHTML = total;
+            }
             break;
         case "times":
             total = parseFloat(input1.value) * parseFloat(input2.value);
-            output.innerHTML = total;
-            console.log(total);
+            if (isNaN(total)) {
+                output.innerHTML = "Where's the Number?";
+            } else {
+                output.innerHTML = total;
+            }
             break;
         case "divided":
             total = parseFloat(input1.value) / parseFloat(input2.value);
             if (input2.value == 0) {
                 output.innerHTML = "Tidak bisa dibagi 0";
+            } else if (isNaN(total)) {
+                output.innerHTML = "Where's the Number?";
             } else {
                 output.innerHTML = total;
             }
             break;
+        default:
+            output.innerHTML = "Invalid operation";
+            return;
     }
+
 
 }
---
+++ assets/js/_323.js
@@ -0,0 +1,10 @@
+// ! For Google, don't disturb!
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-YM5S6613D7');
