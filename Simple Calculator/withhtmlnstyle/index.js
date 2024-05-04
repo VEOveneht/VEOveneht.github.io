346622ad7a51332302a266901608e044e384e605
VEOveneht <lisanshidqi0@gmail.com>
2024-05-04 22:55:21 +0700

    meta and script

--- Simple Calculator/withhtmlnstyle/index.js
+++ Simple Calculator/withhtmlnstyle/index.js
@@ -2,51 +2,43 @@ const input1 = document.querySelector("#input1");
 const input2 = document.querySelector("#input2");
 const output = document.querySelector("#output");
 
-
 function calculateor() {
     let options = document.getElementById("operations").value;
     let total;
 
+    const mindit = () => {
+        console.log(total)
+        if (isNaN(total)) {
+            output.innerHTML = "Where's the Number?";
+        } else {
+            output.innerHTML = total;
+        }
+    }
+
     switch (options) {
         case "add":
             total = parseFloat(input1.value) + parseFloat(input2.value);
-
-            if (isNaN(total)) {
-                output.innerHTML = "Where's the Number?";
-            } else {
-                output.innerHTML = total;
-            }
+            mindit();
             break;
         case "minus":
             total = parseFloat(input1.value) - parseFloat(input2.value);
-            if (isNaN(total)) {
-                output.innerHTML = "Where's the Number?";
-            } else {
-                output.innerHTML = total;
-            }
+            mindit();
+
             break;
         case "times":
             total = parseFloat(input1.value) * parseFloat(input2.value);
-            if (isNaN(total)) {
-                output.innerHTML = "Where's the Number?";
-            } else {
-                output.innerHTML = total;
-            }
+            mindit();
             break;
         case "divided":
             total = parseFloat(input1.value) / parseFloat(input2.value);
-            if (input2.value == 0) {
+            mindit();
+            if (parseFloat(input2.value) == 0) {
                 output.innerHTML = "Tidak bisa dibagi 0";
-            } else if (isNaN(total)) {
-                output.innerHTML = "Where's the Number?";
-            } else {
-                output.innerHTML = total;
             }
+
             break;
         default:
-            output.innerHTML = "Invalid operation";
+            mindit();
             return;
     }
-
-
 }
--- assets/js/_323.js
+++ assets/js/_323.js
@@ -8,3 +8,16 @@ function gtag() {
 gtag('js', new Date());
 
 gtag('config', 'G-YM5S6613D7');
+gtag('set', {
+    'user_id': 'USER_ID'
+});
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-217297355-1');
+gtag('set', {
+    'user_id': 'USER_ID'
+});
