77c31b722f5534da12a635963dc0f7be119e42f2
VEOveneht <lisanshidqi0@gmail.com>
2024-05-04 20:58:12 +0700

    Benerin Style sama Kode yang Numpuk alias Double Code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 const navigation = document.querySelectorAll(".list_nav");
 const autoscroll = document.querySelector(".scrollup");
 const nav = document.querySelector("nav");
 const banner = document.querySelector("#banner");
 const menu = document.querySelector("#menu");
+const mepng = document.querySelector("#mepng");
 const sidebar = document.querySelector(".sidebar");
 const sticky = nav.offsetTop;
 
@@ -17,19 +17,19 @@ function hideSide() {
     menu.style.display = "block"
 
 }
-window.onscroll = function() {
+window.onscroll = function () {
     scroll();
 };
-
-
 
 function scroll() {
     if (window.pageYOffset > sticky) {
         nav.classList.add("sticky");
-        banner.classList.add("banner");
+        banner.style.height = "8vh";
+        mepng.style.filter = "none";
     } else {
         nav.classList.remove("sticky");
-        banner.classList.remove("banner");
+        banner.style.height = "0vh";
+        mepng.style.filter = "drop-shadow(4px 4px 4px black)"
     }
 }
 
@@ -56,63 +56,5 @@ autoscroll.addEventListener("click", () => {
         left: 0,
         behavior: "smooth",
     });
-=======
-const navigation = document.querySelectorAll(".list_nav");
-const autoscroll = document.querySelector(".scrollup");
-const nav = document.querySelector("nav");
-const banner = document.querySelector("#banner");
-const menu = document.querySelector("#menu");
-const sidebar = document.querySelector(".sidebar");
-const sticky = nav.offsetTop;
 
-function showSide() {
-    sidebar.style.width = "100%";
-    menu.style.display = "none"
-}
-
-function hideSide() {
-    sidebar.style.width = "0%";
-    menu.style.display = "block"
-
-}
-window.onscroll = function() {
-    scroll();
-};
-
-
-
-function scroll() {
-    if (window.pageYOffset > sticky) {
-        nav.classList.add("sticky");
-        banner.classList.add("banner");
-    } else {
-        nav.classList.remove("sticky");
-        banner.classList.remove("banner");
-    }
-}
-
-navigation.forEach((item) => {
-    item.addEventListener("mouseenter", () => {
-        navigation.forEach((otherItem) => {
-            if (otherItem !== item) {
-                otherItem.style.color = "#a5a5a5";
-            }
-        });
-    });
-    item.addEventListener("mouseleave", () => {
-        navigation.forEach((otherItem) => {
-            if (otherItem !== item) {
-                otherItem.style.color = "black";
-            }
-        });
-    });
-});
-
-autoscroll.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
->>>>>>> 55b997c4594f1e8aa69eaed658823118a8310cb5
 });
