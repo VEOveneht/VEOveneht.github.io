aee646b4df8354a7d81b73f6b26bed920b22b461
VEOveneht <lisanshidqi0@gmail.com>
2024-05-22 20:40:21 +0700

    Link doang

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -17,20 +17,20 @@ const Contact = () => {
         <div id="contact">
             <form>
                 <label htmlFor="name">Name: </label>
-                <input type="text" id="name" placeholder="Nama"/>
+                <input type="text" id="name" placeholder="Nama" name="nama"/>
                 <label htmlFor="email">Email: </label>
-                <input type="email" id="email" placeholder="Email"/>
+                <input type="email" id="email" placeholder="Email" name="email"/>
                 <label htmlFor="description">Description: </label>
-                <textarea id="description" placeholder="Deskripsi"></textarea>
+                <textarea id="description" placeholder="Deskripsi" name="deskripsi"></textarea>
                 <button type="submit">Submit</button>
             </form>
         </div>
         <div id="sosmed">
             <h1>Social</h1>
             <div>
-                <button href="https://github.com/VEOveneht"><img alt="Github" src={github}/>Github</button>
-                <button href="https://www.instagram.com/loo3837955ol/"><img alt="Instagram" src={instagram}/>Instagram</button>
-                <button href="https://x.com/FOBDBM"><img alt="Twitter" src={twitter}/>Twitter</button>
+                <button ><img alt="Github" src={github}/><a href="https://github.com/VEOveneht">Github</a></button>
+                <button ><img alt="Instagram" src={instagram}/><a href="https://www.instagram.com/loo3837955ol/">Instagram</a></button>
+                <button ><img alt="Twitter" src={twitter}/><a href="https://x.com/FOBDBM">Twitter</a></button>
             </div>
         </div>
     </main>
